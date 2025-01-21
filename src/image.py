277efc63dfe575cc33ace80b9a22ad25ea614279
zhn <zzihan0805@gmail.com>
2025-01-21 01:36:31 -0500

    added logo

--- src/image.py
+++ src/image.py
@@ -7,11 +7,15 @@ class ImageProcessor:
 
     def __str__(self):
         print(self.im.format, self.im.size, self.im.mode)
-        self.im.show()
-    
-    # experiment
-    def pixel_brightness(self):
-        self.
+        self.img.show()
+
+    def open_img(self, image_path):
+        self.img = Image.open(image_path)
+        
+    def assign_ascii(self, pixel_value):
+        # normalize value of pixel to level 0 - 6 for easier ascii conversion
+        normalized_value = float(pixel_value - self.min_pixel) / float(self.max_pixel - self.min_pixel) * (len(self.ascii_chars) - 1)
+        return int(round(normalized_value))
 
     def image_to_ascii(self, image_path, width=100):
         self.img = Image.open(image_path)
@@ -20,17 +24,20 @@ class ImageProcessor:
         height = int(width * aspect_ratio)
         self.img = self.img.resize((width, height))
         pixels = np.array(self.img)
+
+        # find min and max light value for mapping
+        self.min_pixel = pixels.min()
+        self.max_pixel = pixels.max()
         
+        # start empty ascii_str
         ascii_str = ''
         for row in pixels:
-            for pixel in row:
-                pixel_value = self.img.getpixel((row, pixel))
+            for pixel_value in row:
                 ascii_str += self.ascii_chars[self.assign_ascii(pixel_value)]
             ascii_str += '\n'
             
         return ascii_str
     
-    def assign_ascii(self, pixel_value):
-        
+
 
 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
 from image import ImageProcessor
 
 imageProcessor = ImageProcessor()
+logo = imageProcessor.image_to_ascii("../images/AAC_logo.jpg", 100)
+print(('-' * 150 + '\n') * 2)
+print(logo)
+print(('-' * 150 + '\n') * 2)
 
 
+# Get user filename
+filename = input("Enter your file name: ")
+width = input("Enter size of ascii art (in pixels): ")
+
+# Validate width input
+while not width.isdigit():
+    width = input("Size of ascii art must be a number: ")
+
+width = int(width)
+
+processd_image = imageProcessor.image_to_ascii(filename, width)
+print(processd_image)
